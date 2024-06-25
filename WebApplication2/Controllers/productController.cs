9f927c93d78fd1dbfef90265919b3c6f9abe786c
Joe404S <newyorkstoreq@gmail.com>
2024-06-25 18:56:31 +0300

    Add project files.

---
+++ WebApplication2/Controllers/productController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class productController : Controller
+    {
+        public IActionResult Index()
+        {
+            product pr = new product();
+            pr.id = 1;
+            pr.name = "Test";
+            pr.price = 100;
+            pr.number = 123;
+            return View(pr);
+        }
+
+    }
+}
---
+++ WebApplication2/Models/product.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication2.Models
+{
+    public class product
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int price { get; set; }
+        public int number { get; set; }
+    }
+}
